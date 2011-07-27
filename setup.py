479063888bc267bdf31abdc5e86d60a9cf7117da
Marc Tamlyn <marc@incuna.com>
2011-07-27 14:37:30 +0100

    Converted setup.py to setuptools, added MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "txtlocal",
+    name = "incuna-txtlocal",
 	packages = find_packages(),
     include_package_data=True,
     install_requires=[
