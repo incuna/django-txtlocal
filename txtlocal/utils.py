8011edbaba676677c1789541cc9cf463aab5b609
Charlie Denton <charlie@meshy.co.uk>
2013-07-03 12:10:14 +0100

    Completely replace SMS sending code.

--- txtlocal/utils.py
+++ txtlocal/utils.py
@@ -1,156 +1,68 @@
-#!/usr/bin/env python
+import requests
 
-"""
-the main function in this module is sendSMS, which sends an SMS message
-using txtlocal
-"""
-
-import urllib
-import string
-import re
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
+from django.utils.http import urlencode
 
 
-def sendSMS( numbers, message, username, password,
-             proxy = None,
-             allowLongMsg = False,
-             sender = None,
-             **kwargs ):
-
+def send_sms(text, recipient_list, sender=None,
+             username=None, password=None, **kwargs):
     """
-    Send an SMS message
+    Render and send an SMS template.
 
-    Arguments to function are as follows:
+    The current site will be added to any context dict passed.
 
-        numbers: List or phone numbers, or a single phone number.
-                  Each phone number should be an international phone number.
-                  Note that as per the API this should only contain digits
-                  and not other characters.
-                  The API specifies a comma-separated list, but for convenience
-                  this function will also accept a list or tuple in python,
-                  and convert it accordingly.
-        message: The message
-        username: username on txtlocal.co.uk
-        password: password on txtlocal.co.uk
+        recipient_list: A list of number strings. Each number must be purely
+                        numeric, so no '+' symbols, hyphens or spaces. The
+                        numbers must be prefixed with their international
+                        dialling codes.
+                        eg. UK numbers would look like 447123456789.
+        text: The message text. (Gets URI encoded.)
+        sender: Must be word up to 11 characters or a number up to 14 digits.
+                Defaults to settings.DEFAULT_FROM_SMS. (Must be a string.)
+        username: Defaults to settings.TXTLOCAL_USERNAME.
+        password: Defaults to settings.TXTLOCAL_PASSWORD.
+        **kwargs: Will be passed through to textlocal in the POST data.
 
-    Optional arguments:
-
-        proxy: the proxy URL needed to connect to the internet at the client
-               end, e.g. 'http://my.site.proxy:8080'
-
-        allowLongMsg: set to True to allow long message, otherwise messages
-                      will be truncated at 160 chars
-
-        sender: sender name
-
-        anything else: will be passed as-is to the HTTP interface,
-                       as documented on the txtlocal website,
-                       for example:  rcpurl=...
-
-    All arguments (or each phone number in the list) should be of type
-    string, except that integers are accepted and are converted into strings
+    Any unrecognised kwargs will be passed to txtlocal in the POST data.
     """
+    post_data = {
+        'selectednums': '.'.join(recipient_list),
+        'message': urlencode(text),
+        'uname': username or settings.TXTLOCAL_USERNAME,
+        'pword': password or password.TXTLOCAL_PASSWORD,
+        'from': sender or settings.DEFAULT_FROM_SMS,
+        'json': 1,  # This makes textlocal send us back info about the request.
+    }
 
-    # build up dictionary of args
-    dict = _getArgsDict(numbers, message, username, password,
-                        sender, allowLongMsg, kwargs)
-
-    # URL-encode this (note urlencode will accept ints or strings)
-    data = urllib.urlencode(dict)
-
-    # and post
     url = "http://www.txtlocal.com/sendsmspost.php"
-    response = _postURL(url, data, proxy)
+    response = requests.post(url, data=post_data).json()
+    available = response.get('CreditsAvailable')
+    required = response.get('CreditsRequired')
+    remaining = response.get('CreditsRemaining')
+    if (
+        available is None or
+        required is None or
+        remaining is None or
+        required <= 0 or
+        available - required != remaining
+    ):
+        err = 'Message may not have been sent. Response was: "%s"' % response
+        raise RuntimeError(err)
 
-    # check if it seemed to work
-    if not _checkResponse(response):
-        raise RuntimeError("Message may not have been sent. " +
-                           "Response was: '%s'" % response)
 
-def _makeSender(sender):
+def render_to_send_sms(template, context=None, **kwargs):
     """
-    Make an acceptable sender line, else return an empty string
+    Render and send an SMS template.
+
+    The current site will be added to any context dict passed.
+
+    Any unrecognised kwargs will be passed to send_sms.
     """
-    s = ""
-    for char in sender:
-        if char.isalnum() or char == " ":
-            s += char
-    s = s[:11]
-    if len(s) < 3:
-        return ""
-    else:
-        return s
+    if context is None:
+        context = {}
+    context['site'] = Site.objects.get_current()
+    text = render_to_string(template, context)
 
-
-def _getArgsDict(numbers, message, username, password, sender, allowLongMsg,
-                 kwargs):
-    """
-    Helper function for sendSMS - makes dictionary of args according to the
-    API
-    """
-    dict = {}
-    if isinstance(numbers, list) or isinstance(numbers, tuple):
-
-        numbers = map(lambda x: "%s" % x, numbers)  # convert any ints to
-                                                    # strings
-
-        numbers = string.join(numbers, ",")  # and turn into comma separated
-
-    dict['selectednums'] = numbers
-
-    if not allowLongMsg:
-        message = message[:160]
-
-    dict['message'] = message
-
-    dict['uname'] = username
-    dict['pword'] = password
-
-    # any additional args
-    for key in kwargs.keys():
-        dict[key] = kwargs[key]
-
-    if sender:
-        dict['from'] = _makeSender(sender)
-
-    dict['info'] = 1  # always request info, so we can parse the response
-    return dict
-
-
-def _postURL(url, data, proxy):
-    """
-    Helper function for sendSMS - posts request and returns response
-    """
-    if proxy:
-        proxies = { 'http': proxy }
-    else:
-        proxies = None
-    filehandle = urllib.urlopen(url, data = data, proxies = proxies)
-    response = string.join(filehandle.readlines(), "\n")
-    return response
-
-
-def _checkResponse(response):
-    """
-    Helper function for sendSMS - check response text (string) to see if the
-    SMS appears to have been sent - returns True or False
-
-    This is a basic sanity check and doesn't guarantee delivery.  Checks that
-    the report contains some believable numbers regarding credits.
-    """
-    try:
-        credAvail = _parseToken(response,"CreditsAvailable")
-        credReq = _parseToken(response,"CreditsRequired")
-        credRem = _parseToken(response,"CreditsRemaining")
-    except:
-        return False
-
-    return (credReq > 0 and credAvail - credReq == credRem)
-
-
-def _parseToken(response, token):
-    """
-    Helper function for _checkResponse: look for numeric value in response
-    with a given token name
-    """
-    regexp = token + "=([0-9]+)"
-    return string.atoi(re.search(regexp, response).group(1))
+    send_sms(text, **kwargs)
