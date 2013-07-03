de0e0aed50e79864249e756ad369803aaf60ab01
Charlie Denton <charlie@meshy.co.uk>
2013-07-03 12:48:46 +0100

    Semi-pin version of requirement.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-txtlocal',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['requests'],
+    install_requires=['requests>=1.2.3'],
     version='1.0',
     description='App for sending and receiving SMS messages via http://www.textlocal.com',
     author='Incuna Ltd',
