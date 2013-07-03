401e64258e926f51763d90eebf292161147e7fe0
Charlie Denton <charlie@meshy.co.uk>
2013-07-03 14:51:09 +0100

    Do versioning correctly, and add CHANGELOG.

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+Changelog for django-incuna
+===========================
+
+v0.2
+------
+* Rewrite SMS sending code.
+* Rename ``incuna-txtlocal`` to ``django-txtlocal``.
+* PEP8.
+* Fiddle with models a bit.
+* Add README, CHANGELOG and LICENCE.
+* Migrate to git.
+
+v0.1
+------
+* Initial release.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=['requests>=1.2.3'],
-    version='1.0',
+    version='0.2',
     description='App for sending and receiving SMS messages via http://www.textlocal.com',
     author='Incuna Ltd',
     author_email='admin@incuna.com',
