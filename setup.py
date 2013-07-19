46ffd6af4346700f7e0f199faa77ae14b03bb729
George Hickman <george@ghickman.co.uk>
2013-07-19 13:11:48 +0100

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=['requests>=1.2.3'],
-    version='1.1',
+    version='1.1.1',
     description='App for sending and receiving SMS messages via http://www.textlocal.com',
     long_description=open('README.rst').read(),
     author='Incuna Ltd',
